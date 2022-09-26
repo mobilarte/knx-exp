49220b3e0af5ec5ed3a2a7bc3a6522840768e2bb
mobilarte <martin@maxsyma.com>
2022-09-26 17:31:26 +0200

    Added DPT 20.105

--- knx/dpt/types_20.go
+++ knx/dpt/types_20.go
@@ -4,11 +4,11 @@ package dpt
 type DPT_20102 uint8
 
 const (
-	Auto               DPT_20102 = 0
-	Comfort            DPT_20102 = 1
-	Standby            DPT_20102 = 2
-	Economy            DPT_20102 = 3
-	BuildingProtection DPT_20102 = 4
+	HVACMode_Auto               DPT_20102 = 0
+	HVACMode_Comfort            DPT_20102 = 1
+	HVACMode_Standby            DPT_20102 = 2
+	HVACMode_Economy            DPT_20102 = 3
+	HVACMode_BuildingProtection DPT_20102 = 4
 )
 
 func (d DPT_20102) Pack() []byte {
@@ -24,21 +24,107 @@ func (d DPT_20102) Unit() string {
 }
 
 func (d DPT_20102) IsValid() bool {
-	return d <= BuildingProtection
+	return d <= HVACMode_BuildingProtection
 }
 
 func (d DPT_20102) String() string {
 	switch d {
-	case Auto:
+	case HVACMode_Auto:
 		return "Auto"
-	case Comfort:
+	case HVACMode_Comfort:
 		return "Comfort"
-	case Standby:
+	case HVACMode_Standby:
 		return "Standby"
-	case Economy:
+	case HVACMode_Economy:
 		return "Economy"
-	case BuildingProtection:
+	case HVACMode_BuildingProtection:
 		return "Building Protection"
+	default:
+		return "reserved"
+	}
+}
+
+// DPT_20102 represents DPT 20.105 (HVAC) / DPT_HVACContrMode.
+type DPT_20105 uint8
+
+const (
+	HVACContrMode_Auto                    DPT_20105 = 0
+	HVACContrMode_Heat                    DPT_20105 = 1
+	HVACContrMode_Morning_Pump            DPT_20105 = 2
+	HVACContrMode_Cool                    DPT_20105 = 3
+	HVACContrMode_Night_Purge             DPT_20105 = 4
+	HVACContrMode_Precool                 DPT_20105 = 5
+	HVACContrMode_Off                     DPT_20105 = 6
+	HVACContrMode_Test                    DPT_20105 = 7
+	HVACContrMode_Emergency_Heat          DPT_20105 = 8
+	HVACContrMode_Fan_only                DPT_20105 = 9
+	HVACContrMode_Free_Cool               DPT_20105 = 10
+	HVACContrMode_Ice                     DPT_20105 = 11
+	HVACContrMode_Maximum_Heating_Mode    DPT_20105 = 12
+	HVACContrMode_Economic_Heat_Cool_Mode DPT_20105 = 13
+	HVACContrMode_Dehumidification        DPT_20105 = 14
+	HVACContrMode_Calibration_Mode        DPT_20105 = 15
+	HVACContrMode_Emergency_Cool_Mode     DPT_20105 = 16
+	HVACContrMode_Emergency_Steam_Mode    DPT_20105 = 17
+	HVACContrMode_NoDem                   DPT_20105 = 20
+)
+
+func (d DPT_20105) Pack() []byte {
+	return packU8(uint8(d))
+}
+
+func (d *DPT_20105) Unpack(data []byte) error {
+	return unpackU8(data, (*uint8)(d))
+}
+
+func (d DPT_20105) Unit() string {
+	return ""
+}
+
+func (d DPT_20105) IsValid() bool {
+	return d <= HVACContrMode_NoDem && uint8(d) != 18 && uint8(d) != 19
+}
+
+func (d DPT_20105) String() string {
+	switch d {
+	case HVACContrMode_Auto:
+		return "Auto"
+	case HVACContrMode_Heat:
+		return "Heat"
+	case HVACContrMode_Morning_Pump:
+		return "Morning Pump"
+	case HVACContrMode_Cool:
+		return "Cool"
+	case HVACContrMode_Night_Purge:
+		return "Night Purge"
+	case HVACContrMode_Precool:
+		return "Precool"
+	case HVACContrMode_Off:
+		return "Off"
+	case HVACContrMode_Test:
+		return "Test"
+	case HVACContrMode_Emergency_Heat:
+		return "Emergency Heat"
+	case HVACContrMode_Fan_only:
+		return "Fan only"
+	case HVACContrMode_Free_Cool:
+		return "Free Cool"
+	case HVACContrMode_Ice:
+		return "Ice"
+	case HVACContrMode_Maximum_Heating_Mode:
+		return "Maximum Heating Mode"
+	case HVACContrMode_Economic_Heat_Cool_Mode:
+		return "Economic Heat/Cool Mode"
+	case HVACContrMode_Dehumidification:
+		return "Dehumidification"
+	case HVACContrMode_Calibration_Mode:
+		return "Calibration Mode"
+	case HVACContrMode_Emergency_Cool_Mode:
+		return "Emergency Cool Mode"
+	case HVACContrMode_Emergency_Steam_Mode:
+		return "Emergency Steam Mode"
+	case HVACContrMode_NoDem:
+		return "NoDem"
 	default:
 		return "reserved"
 	}
--- knx/dpt/types_20_test.go
+++ knx/dpt/types_20_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-// Test DPT 20.xxx (N8)
+// Test DPT 20.102 (N8)
 func TestDPT_20102(t *testing.T) {
 	type DPT20 struct {
 		Dpv DatapointValue
@@ -19,6 +19,7 @@ func TestDPT_20102(t *testing.T) {
 		{new(DPT_20102), 2, "Standby"},
 		{new(DPT_20102), 3, "Economy"},
 		{new(DPT_20102), 4, "Building Protection"},
+		{new(DPT_20102), 5, "reserved"},
 	}
 	for _, e := range types_20 {
 		err := e.Dpv.Unpack(packU8(e.Idx))
@@ -31,16 +32,46 @@ func TestDPT_20102(t *testing.T) {
 	}
 }
 
-/*
-	knxValue := []byte{0, 4}
-	dptValue := DPT_20102(4)
+// Test DPT 20.105 (N8)
+func TestDPT_20105(t *testing.T) {
+	type DPT20 struct {
+		Dpv DatapointValue
+		Idx uint8
+		Val string
+	}
 
-	var tmpDPT DPT_20102
-	assert.NoError(t, tmpDPT.Unpack(knxValue))
-	assert.Equal(t, dptValue, tmpDPT)
+	var types_20 = []DPT20{
+		{new(DPT_20105), 0, "Auto"},
+		{new(DPT_20105), 1, "Heat"},
+		{new(DPT_20105), 2, "Morning Pump"},
+		{new(DPT_20105), 3, "Cool"},
+		{new(DPT_20105), 4, "Night Purge"},
+		{new(DPT_20105), 5, "Precool"},
+		{new(DPT_20105), 6, "Off"},
+		{new(DPT_20105), 7, "Test"},
+		{new(DPT_20105), 8, "Emergency Heat"},
+		{new(DPT_20105), 9, "Fan only"},
+		{new(DPT_20105), 10, "Free Cool"},
+		{new(DPT_20105), 11, "Ice"},
+		{new(DPT_20105), 12, "Maximum Heating Mode"},
+		{new(DPT_20105), 13, "Economic Heat/Cool Mode"},
+		{new(DPT_20105), 14, "Dehumidification"},
+		{new(DPT_20105), 15, "Calibration Mode"},
+		{new(DPT_20105), 16, "Emergency Cool Mode"},
+		{new(DPT_20105), 17, "Emergency Steam Mode"},
+		{new(DPT_20105), 18, "reserved"},
+		{new(DPT_20105), 19, "reserved"},
+		{new(DPT_20105), 20, "NoDem"},
+		{new(DPT_20105), 21, "reserved"},
+	}
 
-	assert.Equal(t, knxValue, dptValue.Pack())
-
-	assert.Equal(t, "Building Protection", dptValue.String())
+	for _, e := range types_20 {
+		err := e.Dpv.Unpack(packU8(e.Idx))
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+		if fmt.Sprintf("%s", e.Dpv) != e.Val {
+			t.Errorf("%#v has wrong value [%v]. Should be [%s].", e.Dpv, e.Dpv, e.Dpv)
+		}
+	}
 }
-*/
