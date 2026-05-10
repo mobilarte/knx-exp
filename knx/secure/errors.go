a14f2e1429c9716ec7b7156e1cfe57fd68a4ac1c
mobilarte <martin@maxsyma.com>
2026-05-10 18:47:10 +0200

    feat: add error types for Data Secure operations

---
+++ knx/secure/errors.go
@@ -0,0 +1,26 @@
+package secure
+
+import "fmt"
+
+// DataSecureError represents an error during Data Secure operations
+type DataSecureError struct {
+	Message string
+	Cause   error
+}
+
+func (e *DataSecureError) Error() string {
+	if e.Cause != nil {
+		return fmt.Sprintf("data secure error: %s: %v", e.Message, e.Cause)
+	}
+	return fmt.Sprintf("data secure error: %s", e.Message)
+}
+
+// NewDataSecureError creates a new DataSecureError
+func NewDataSecureError(message string) *DataSecureError {
+	return &DataSecureError{Message: message}
+}
+
+// NewDataSecureErrorWithCause creates a new DataSecureError with a cause
+func NewDataSecureErrorWithCause(message string, cause error) *DataSecureError {
+	return &DataSecureError{Message: message, Cause: cause}
+}
