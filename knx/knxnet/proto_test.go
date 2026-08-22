60c9516151c90bbfe3a969a4c7f0b620d0f65649
mobilarte <martin@maxsyma.com>
2026-08-22 19:52:08 +0200

    fix golangci-lint embedlit

--- knx/knxnet/proto_test.go
+++ knx/knxnet/proto_test.go
@@ -13,22 +13,22 @@ import (
 func BenchmarkPack(b *testing.B) {
 	b.ReportAllocs()
 
+	lData := cemi.LData{
+		Control1: cemi.Control1NoRepeat | cemi.Control1NoSysBroadcast |
+			cemi.Control1StdFrame | cemi.Control1WantAck | cemi.Control1Prio(cemi.PrioLow),
+		Control2:    cemi.Control2GroupAddr | cemi.Control2Hops(6),
+		Source:      0,
+		Destination: 0x1337,
+		Data: &cemi.AppData{
+			Command: cemi.GroupValueWrite,
+			Data:    []byte{0, 0x13, 0x37},
+		},
+	}
+
 	req := &TunnelReq{
 		Channel:   1,
 		SeqNumber: 0,
-		Payload: &cemi.LDataReq{
-			LData: cemi.LData{
-				Control1: cemi.Control1NoRepeat | cemi.Control1NoSysBroadcast |
-					cemi.Control1StdFrame | cemi.Control1WantAck | cemi.Control1Prio(cemi.PrioLow),
-				Control2:    cemi.Control2GroupAddr | cemi.Control2Hops(6),
-				Source:      0,
-				Destination: 0x1337,
-				Data: &cemi.AppData{
-					Command: cemi.GroupValueWrite,
-					Data:    []byte{0, 0x13, 0x37},
-				},
-			},
-		},
+		Payload:   &cemi.LDataReq{LData: lData},
 	}
 
 	for b.Loop() {
